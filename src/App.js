0d524442390ce7294cccf847af827ccce662d5f8
Kristina Karvanova <kristaliska@gmail.com>
2023-01-19 14:36:29 +0200

    new_table_with_sorting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
+import Table from "./Table/Table.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      sort: 'asc',
+      sortField: 'id'
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}")
+      .then(res => res.json())
+      .then(
+        (res) => {
+          this.setState({
+            isLoaded: true,
+            items: res
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  onSort = sortField => {
+    let copy = Object.assign([], this.state.items);
+    if(this.state.sort === 'asc') {
+      copy.sort((a, b) => a[sortField] > b[sortField] ? 1 : -1);
+      this.setState({
+        sort: 'desc',
+        sortField: sortField,
+        items: copy
+      });
+    } else {
+      copy.sort((a, b) => b[sortField] > a[sortField] ? 1 : -1);
+      this.setState({
+        sort: 'asc',
+        sortField: sortField,
+        items: copy
+      });
+    }
+  }
+
+  render() {
+    const { error, isLoaded, items, sort, sortField} = this.state;
+
+    if (error) {
+      return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>;
+    } else {
+      return (
+        <Table items={items}
+          onSort={this.onSort}
+          sort={sort}
+          sortField={sortField}
+        />
+      ) 
+     
+    }
+  }
+
 }
 
 export default App;
